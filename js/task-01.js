6a7448fc5b31c764df93574a571f0285a96c0eb3
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-06-14 18:43:27 +0300

    Completed 01-03.js.tasks

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories,
+// тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories,
+// знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
+// і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод forEach()
+// і властивості навігації по DOM.
+
+const list = document.querySelector("#categories"); // повертає перший елемент документа, який відповідає вказаному селектору
+console.log(list);
+
+console.log(`Number of categories: ${list.children.length}`); // властивість, яка повертає HTML-колекцію(псевдомасив) усіх дітей елемента(включає тільки вузли-елементи, тобто теги), на якому була викликана
+
+[...list.children].forEach((li) => {
+  console.log(`Category: ${li.firstElementChild.textContent}`);
+  console.log(`Elements: ${li.lastElementChild.children.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const ingredientsList = document.querySelector("#ingredients");
+console.log(ingredientsList);
+
+const ingredientsMarkup = ingredients.map((ingredient) => {
+  const li = document.createElement("li"); // сторює елемент ('TagName') і повертає посилання на нього як результат свого виконання
+  li.textContent = ingredient;
+  li.classList.add("item");
+
+  return li;
+});
+
+console.log(ingredientsMarkup);
+
+ingredientsList.append(...ingredientsMarkup); // додає один або декілька елементів після всіх дітей елемента
