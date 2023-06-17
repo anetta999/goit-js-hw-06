9f4b422f86239ec3f194037d6b5c303fd17f6f71
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-06-17 12:02:29 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 // Створи функцію createBoxes(amount), яка приймає один параметр - число.
 // Функція створює стільки < div >,
 // скільки вказано в amount і додає їх у div#boxes.
@@ -20,28 +14,48 @@ const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 console.log(createBtn);
 console.log(destroyBtn);
-const input = createBtn.previousElementSibling;
+const input = document.querySelector("#controls input");
 console.log(input);
 const boxes = document.querySelector("#boxes");
 
-function createBoxes(amount) {
-  amount = Number(input.value);
+createBtn.addEventListener("click", handlerCreateBtnClick);
 
-  createBtn.addEventListener("click", () => {
+function handlerCreateBtnClick() {
+  const amount = Number(input.value);
+
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  const newBoxes = [];
+  let step = 0;
+
+  for (let i = 0; i <= amount; i += 1) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("box");
     newDiv.style.backgroundColor = getRandomHexColor();
 
-    let step = 0;
+    newDiv.style.width = `${step}px`;
+    newDiv.style.height = `${step}px`;
     step += 10;
-    newDiv.style.width = `${step}`;
-    newDiv.style.height = `${step}`;
-    boxes.append(newDiv);
-  });
+
+    newBoxes.push(newDiv);
+  }
+  boxes.append(...newBoxes);
+}
+
+destroyBtn.addEventListener("click", handlerDestroyBtnClick);
+
+function handlerDestroyBtnClick() {
+  destroyBoxes();
 }
 
 function destroyBoxes() {
-  destroyBtn.addEventListener("click", () => {
-    boxes.innerHTML = "";
-  });
+  boxes.innerHTML = "";
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
